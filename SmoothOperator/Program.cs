4a17269c9920e35f221867fc4c992ccfc79c6195
Steven Hall <stevenbryanb.work@gmail.com>
2024-02-28 18:45:53 +0000

    Added Convertion and readline

--- SmoothOperator/Program.cs
+++ SmoothOperator/Program.cs
@@ -14,8 +14,14 @@ namespace SmoothOperator
         private static void Main(string[] args)
         {
 
-            // request byte integer
+            // request byte input
             Console.Write("Insere um número inteiro não-negativo: ");
+
+            // byte conversion from string
+            byte n; 
+
+            // converion & readline
+            n = byte.Parse(Console.Readline()); 
 
         }
     }
