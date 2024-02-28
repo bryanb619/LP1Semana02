d5c53467910d4211df3bc3f6bdeaf5bc60b424a7
Steven Hall <stevenbryanb.work@gmail.com>
2024-02-28 19:21:00 +0000

    Added ValoresEspeciais Project
    Added sbyte min & max value

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            // sbyte
+            Console.WriteLine(sbyte.MinValue);
+            Console.WriteLine(sbyte.MaxValue);
+
+
+        }
+    }
+}
