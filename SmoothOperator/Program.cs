229eeaa00057c71f4e1151b36ffdb43e996fafcf
Steven Hall <stevenbryanb.work@gmail.com>
2024-02-28 18:38:24 +0000

    Added Smooth Operator project
    Added Text request for user

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args">Not used</param>
+        private static void Main(string[] args)
+        {
+
+            // request byte integer
+            Console.Write("Insere um número inteiro não-negativo: ");
+
+        }
+    }
+}
