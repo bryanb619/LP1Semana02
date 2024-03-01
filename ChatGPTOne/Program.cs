5920449c0f6198973ae597ee3b4c985f3625073a
Steven Hall <stevenbryanb.work@gmail.com>
2024-03-01 12:29:09 +0000

    Added switch with full functionality
    Added EXIT condition to while statement
    
    Added default statent

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -1,43 +1,72 @@
 ﻿using System;
+using System.Net;
 
 namespace ChatGPTOne
 {
     class Program
     {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
-           while(true)
+
+            string input = "", response; 
+    
+        
+
+           while(input != "EXIT")
+
            {
-                string input = Console.ReadLine(); 
+        
+                Console.Write
+                ("Hi I am a very smart ai, please ask me things!: "); 
+
+                input = Console.ReadLine(); 
+                
 
                 switch(input)
                 {
                     case "are you amazing?":
                     {   
-
+                        response = "extremely";
                         break; 
                     }
 
                     case "is earth round or square?":
                     {   
-
+                        response = "Round...";
                         break; 
                     }
-                    case "":
+                    case "do you like ai?":
                     {   
-
+                        response = "Yes I'm one";
                         break; 
                     }
                     case "do you like c#?":
                     {   
-
+                        response = "No, I like Python 3.6.1";
                         break; 
                     }
                     case "EXIT":
                     {
-                        break
+                        response = "EXITING";
+                        break;
+    
+            
                     }
+                    default:
+                    {
+                        response = "I could not understand,\nplease try again!";
+                        break; 
+                    }
+
+               
                 }
+
+                Console.WriteLine(response); 
            }
         }
     }
