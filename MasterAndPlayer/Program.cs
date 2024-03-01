787e82b6714130bfac95b81f9f9991045d6c3906
Steven Hall <stevenbryanb.work@gmail.com>
2024-03-01 13:27:33 +0000

    Added while & better comments

--- MasterAndPlayer/Program.cs
+++ MasterAndPlayer/Program.cs
@@ -14,6 +14,7 @@ namespace MasterAndPlayer
             // integer number conversion & input read
             int n = int.Parse(Console.ReadLine());
 
+            // check if lower or higher then 100
             if (n < 0 || n> 100)
             {
                 Console.WriteLine
@@ -21,13 +22,28 @@ namespace MasterAndPlayer
             }
             else
             {
+                // print nothing 100 times
                 for (int j = 0; j <=100; j++)
                 {
                     Console.WriteLine("");
                 }
 
+
+                while(true)
+                {
+                    // request input
+                    Console.Write("Player insert number"); 
+                    // integer number conversion & input read
+                    int i = int.Parse(Console.ReadLine());
+
+                    if(i < 0 || i> 100)
+                    {
+
+                    }
+
+                }
                 
-                Console.Write("Player insert number"); 
+
 
 
                 
