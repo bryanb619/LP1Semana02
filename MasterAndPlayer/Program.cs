e5736dac93cf169510a08ec35ba1f2a9134224e5
Steven Hall <stevenbryanb.work@gmail.com>
2024-03-01 13:09:25 +0000

    Added MasterAndPlayer

---
+++ MasterAndPlayer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MasterAndPlayer
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
