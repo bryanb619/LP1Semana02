d1dee506e025f3edf86f0dbba8b3ab87687a3215
Steven Hall <stevenbryanb.work@gmail.com>
2024-03-01 13:19:35 +0000

    Added for loop

--- MasterAndPlayer/Program.cs
+++ MasterAndPlayer/Program.cs
@@ -7,14 +7,24 @@ namespace MasterAndPlayer
         private static void Main(string[] args)
         {
 
-            //
+            // request input
             Console.WriteLine("Insere um número inteiro entre 0 e 100");
 
             // integer number conversion & input read
             int n = int.Parse(Console.ReadLine());
 
-            if (n < 0)
+            if (n < 0 || n> 100)
             {
+                Console.WriteLine
+                ("Erro, número inserido deve ser entre 0 e 100");
+            }
+            else
+            {
+                for (int j = 0; j <=100; j++)
+                {
+                    Console.WriteLine("");
+                }
+
 
             }
 
