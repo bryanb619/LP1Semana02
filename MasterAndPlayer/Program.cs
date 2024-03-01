0895b0d5bc845a6cf383b1cd3f9c735bf3c7f72e
Steven Hall <stevenbryanb.work@gmail.com>
2024-03-01 13:47:49 +0000

    Removed unecessary else statement

--- MasterAndPlayer/Program.cs
+++ MasterAndPlayer/Program.cs
@@ -21,66 +21,64 @@ namespace MasterAndPlayer
                 // print error message
                 Console.WriteLine
                 ("Invalid number! Try again!");
+                return; 
             }
 
-            else
+            // print nothing 100 times
+            for (int j = 0; j <=100; j++)
             {
-                // print nothing 100 times
-                for (int j = 0; j <=100; j++)
-                {
-                    // print nothing
-                    Console.WriteLine("");
-                }
-
-                // game loop
-                while(!playerWins)
-                {
-                    // request input
-                    Console.Write("Player insert number: "); 
-
-                    // integer number conversion & input read
-                    int i = int.Parse(Console.ReadLine());
-
-                    // check if i is lower or higher then 100
-                    if(i < 0 || i> 100)
-                    {   
-                        // print error message
-                        Console.WriteLine("Invalid number! Try Again!"); 
-                 
-                    }
-
-                    
-                    else
-                    {
-                        // check if i is lower then n
-                        if (i < n)
-                        {
-                            // print hint message
-                            Console.WriteLine
-                            ($"Valid number is higher then {i}");
-                        }
-
-                        // check if i higher then n 
-                        else if(i > n)
-                        {
-                            // print hint message
-                            Console.WriteLine
-                            ($"Valid number is lower then {i}");
-
-                        }
-
-                        // Player wins condition
-                        else if (i == n)
-                        {
-                            Console.WriteLine("Player wins"); 
-                            playerWins = true; 
-                        
-                        }
-
-                    }
-
-                }
+                // print nothing
+                Console.WriteLine("");
             }
+
+            // game loop
+            while(!playerWins)
+            {
+                // request input
+                Console.Write("Player insert number: "); 
+
+                // integer number conversion & input read
+                int i = int.Parse(Console.ReadLine());
+
+                // check if i is lower or higher then 100
+                if(i < 0 || i> 100)
+                {   
+                    // print error message
+                    Console.WriteLine("Invalid number! Try Again!"); 
+                
+                }
+
+                else
+                {
+                    // check if i is lower then n
+                    if (i < n)
+                    {
+                        // print hint message
+                        Console.WriteLine
+                        ($"Valid number is higher then {i}");
+                    }
+
+                    // check if i higher then n 
+                    else if(i > n)
+                    {
+                        // print hint message
+                        Console.WriteLine
+                        ($"Valid number is lower then {i}");
+
+                    }
+
+                    // Player wins condition
+                    else if (i == n)
+                    {
+                        Console.WriteLine("Player wins"); 
+                        playerWins = true; 
+                    
+                    }
+
+                }
+
+            }
+            
         }
     }
 }
